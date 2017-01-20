4fdbcad804a03398ea85613f662ee032a6ecb0b7
Matthew <mdudd001@gold.ac.uk>
2017-01-20 09:45:50 +0000

    Base for Vector3 class (can we do this as header only?)

---
+++ Camera_Path/Source/Camera_Path/Vector3.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Camera_Path.h"
+#include "Vector3.h"
+
+Vector3::Vector3()
+{
+}
+
+Vector3::~Vector3()
+{
+}
---
+++ Camera_Path/Source/Camera_Path/Vector3.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class CAMERA_PATH_API Vector3
+{
+private:
+
+  float x_ = 0;
+  float y_ = 0;
+  float z_ = 0;
+
+public:
+
+  Vector3(float x, float y, float z) {
+    x_ = x;
+    y_ = y;
+    z_ = z;
+  };
+
+  ~Vector3() {
+    x_ = 0;
+    y_ = 0;
+    z_ = 0;
+  };
+
+  float& x() {
+    return x_;
+  }
+
+  float& y() {
+    return y_;
+  }
+
+  float& z() {
+    return z_;
+  }
+
+};
