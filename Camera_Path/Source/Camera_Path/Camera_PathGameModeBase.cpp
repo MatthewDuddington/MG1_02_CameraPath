f1f25536515d1ed6488065bcdbd5767e63252007
Matthew <mdudd001@gold.ac.uk>
2017-01-20 09:44:57 +0000

    Unreal project setup files

---
+++ Camera_Path/Source/Camera_Path/Camera_PathGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Camera_Path.h"
+#include "Camera_PathGameModeBase.h"
+
+
+
+
---
+++ Camera_Path/Source/Camera_Path/Camera_PathGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Camera_PathGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAMERA_PATH_API ACamera_PathGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
