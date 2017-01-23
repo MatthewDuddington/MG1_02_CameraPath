b5b9dd2608c00642b7fa85c3196520f5e5965e9f
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-23 18:04:57 +0000

    Started the 4x4 matrix class

---
+++ Camera_Path/Source/Camera_Path/Matrix4.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Camera_Path.h"
+#include "Matrix4.h"
+
+Matrix4::Matrix4()
+{
+}
+
+Matrix4::~Matrix4()
+{
+}
