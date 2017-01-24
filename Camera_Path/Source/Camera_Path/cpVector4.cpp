de34cfb0a7451d3eb0b4689c1417428a3f30bb6f
Matthew <mdudd001@gold.ac.uk>
2017-01-24 18:36:39 +0000

    Adding vector4 classes

---
+++ Camera_Path/Source/Camera_Path/cpVector4.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Camera_Path.h"
+#include "cpVector4.h"
+
+cpVector4::cpVector4()
+{
+}
+
+cpVector4::~cpVector4()
+{
+}
---
+++ Camera_Path/Source/Camera_Path/cpVector4.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class CAMERA_PATH_API cpVector4
+{
+public:
+	cpVector4();
+	~cpVector4();
+};
