811a3b2018b12e0d6ea0278538104df7f5272696
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-26 12:26:11 +0000

    adding the basic movement and lookat function in unity, use the example_cameraLookat scene to view the implementation

---
+++ Camera_Path_Unity/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour {
+
+    public GameObject pOfI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(pOfI.transform);
+    }
+}
--- Camera_Path_Unity/Assets/Scripts/CameraMovement.cs
+++ Camera_Path_Unity/Assets/Scripts/CameraMovement.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject[] way_points;
+    public int wp_id; 
+
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
+    public float distCovered;
+
+    void Start()
+    {
+      
+        startTime = Time.time;
+        //journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+    void Update()
+    {
+        if (transform.position == way_points[wp_id+1].transform.position)
+        {  
+            wp_id++;
+            startTime = Time.time;
+            if (wp_id == way_points.Length-1)
+            {
+                wp_id = 0;
+            }
+        }
+
+        distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / Vector3.Distance(way_points[wp_id].transform.position, way_points[wp_id+1].transform.position);
+        transform.position = Vector3.Lerp(way_points[wp_id].transform.position,way_points[wp_id+1].transform.position, fracJourney);
+    }
 }
