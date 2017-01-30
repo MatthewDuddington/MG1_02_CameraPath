a851914c1f0d8247fe5849d66bb57521f27fb3d3
Matthew Duddington <mdudd001@gold.ac.uk>
2017-01-30 20:38:43 +0000

    Linear point of interest bias based on distance from point of interest when within area of interest

--- Camera_Path_Unity/Assets/Scripts/CameraLook.cs
+++ Camera_Path_Unity/Assets/Scripts/CameraLook.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour {
 
-    public GameObject[] pOfI;
-    public int pOfI_ID = 0;
+    public PointOfInterest[] pois;
+    public int poi_ID = 0;
+    public GameObject PoIBlob;
 
     public int timer = 0;
 
@@ -17,12 +18,52 @@ public class CameraLook : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-       if(timer == 299){
-        pOfI_ID++;
-       }
+//       if(timer == 299){
+//        pOfI_ID++;
+//       }
+//
+//       transform.LookAt(pOfI[pOfI_ID].transform);
+//       timer++;
+//       timer = timer % 300;
 
-       transform.LookAt(pOfI[pOfI_ID].transform);
-       timer++;
-       timer = timer % 300;
+
+
+		// Check for camera being close enough to recieve rotation influence from each point of interest
+		float[] weights = new float[pois.Length];
+		for (int i = 0; i < pois.Length; i++) {
+			float distanceFromPoI = Vector3.Magnitude(pois[i].transform.position - transform.position);
+			print (distanceFromPoI);
+			if (distanceFromPoI <= pois[i].AreaOfInterestRadius()) {
+				weights[i] = 1 - (distanceFromPoI / pois[i].AreaOfInterestRadius());
+			}
+			else {
+				weights[i] = 0;
+			}
+		}
+
+		// Get the total amount of influence being cast upon the camera
+		float weightTotal = 0;
+		foreach (float weight in weights) {
+			weightTotal += weight;
+		}
+
+		// Normalise the weights
+		for (int i = 0; i < weights.Length; i++) {
+			weights[i] = weights[i] / weightTotal;
+		}
+
+		// Create compound Vector3 to look at
+		Vector3 compoundPoI = new Vector3();
+		for (int i = 0; i < pois.Length; i++) {
+			compoundPoI += pois[i].transform.position * weights[i];
+		}
+
+		// Look at the compound point
+		PoIBlob.transform.position = compoundPoI;
+		transform.LookAt(compoundPoI);
+
     }
+
+
+
 }
---
+++ Camera_Path_Unity/Assets/Scripts/PointOfInterest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour {
+
+	private float areaOfInterestRadius;
+
+	void Start() {
+		areaOfInterestRadius = GetComponent<SphereCollider>().radius;
+	}
+
+	public float AreaOfInterestRadius() {
+		return areaOfInterestRadius;
+	}
+
+
+
+
+}
