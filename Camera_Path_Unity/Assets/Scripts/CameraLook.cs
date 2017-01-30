a9e53fef9cf6db1d5cbc54b96de3956f0212eaa2
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-30 18:37:19 +0000

    adding a different layout and a dynamic array of points of interest

--- Camera_Path_Unity/Assets/Scripts/CameraLook.cs
+++ Camera_Path_Unity/Assets/Scripts/CameraLook.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour {
 
-    public GameObject pOfI;
+    public GameObject[] pOfI;
+    public int pOfI_ID = 0;
+
+    public int timer = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,13 @@ public class CameraLook : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(pOfI.transform);
+
+       if(timer == 299){
+        pOfI_ID++;
+       }
+
+       transform.LookAt(pOfI[pOfI_ID].transform);
+       timer++;
+       timer = timer % 300;
     }
 }
