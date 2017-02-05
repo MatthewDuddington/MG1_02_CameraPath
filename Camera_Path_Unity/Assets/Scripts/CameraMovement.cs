0e29fbca09abc02d5c6b6220f858dc750ac2b358
DESKTOP-BVN8U5J\Robert Doig <robertjdoig@hotmail.co.uk>
2017-02-05 18:35:35 +0000

    - Removed the scriptable objects as they werent keeping the references of the waypoints.
    - Also added c1 constraints onto the bezier curve, its toggled with the variable in the inspector

--- Camera_Path_Unity/Assets/Scripts/CameraMovement.cs
+++ Camera_Path_Unity/Assets/Scripts/CameraMovement.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public enum movementType {lerp, bezier, bSpline, hermit, catmullRom}
+
+    [Header("Movement Variables")]
+    public movementType current_movement; 
+
     public MovementType movement;
 
     public GameObject wp_holder;
     private GameObject[] waypoints;
+
+    [Header("Lerp Specific")]
+    public float l_move_speed = 10;
+
+    [Header("Bezier Specific")]
+    public float b_move_speed = 0.5f;
+    public bool use_c1 = false;
+
+    [Header("bSpline Specific")]
+    public float bS_move_speed = 0.5f;
+
+    [Header("Hermit Specific")]
+    public float h_move_speed = 0.5f;
+    public float angular_offset = 30;
+
+    [Header("Catmull-Rom Specific")]
+    public float c_move_speed = 0.5f;
+ 
 
     void Start()
     {
@@ -15,8 +38,24 @@ public class CameraMovement : MonoBehaviour
         for (int i = 0; i != waypoints.Length; i++)
             waypoints[i] = wp_holder.transform.GetChild(i).gameObject;
 
-        movement.reset();
-        movement.setWp(waypoints);
+        switch (current_movement)
+        {
+            case movementType.lerp:
+                movement = new LerpMovement(waypoints, l_move_speed);
+                break;
+            case movementType.bezier:
+                movement = new BezierMovement(waypoints, b_move_speed, use_c1);
+                break;
+            case movementType.bSpline:
+                movement = new BSplineMovement(waypoints, bS_move_speed);
+                break;
+            case movementType.hermit:
+                movement = new HermitMovement(waypoints, h_move_speed, angular_offset);
+                break;
+            case movementType.catmullRom:
+                movement = new CatmullMovement(waypoints, c_move_speed);
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -26,18 +65,15 @@ public class CameraMovement : MonoBehaviour
     }
 }
 
-public abstract class MovementType : ScriptableObject
+public abstract class MovementType
 {
-    protected GameObject[] waypoints;
+    protected GameObject[] wps;
     protected float startTime;
     protected float distCovered;
     protected int wp_id;
     protected Curve curveType;
 
-    [Header("Path Control")]
-    public float speed = 0.5f;
-
-    [Header("Debug Line")]
+    public float speed;
     public float debug_drawing_step = 0.01f;
     public float debug_duration = 1;
 
@@ -45,17 +81,10 @@ public abstract class MovementType : ScriptableObject
 
     public void reset()
     {
-        startTime = 0;
-        wp_id = 0;
+        startTime = Time.time;
         distCovered = 0;
+        wp_id = 0;
     }
-
-    public void setWp(GameObject[] wp)
-    {
-        waypoints = new GameObject[wp.Length];
-        waypoints = wp;
-    }
-
 
     public abstract Vector3 moveCamera();
 
@@ -66,7 +95,7 @@ public abstract class MovementType : ScriptableObject
             MonoBehaviour.print("Updating Wp_id:.." + wp_id);
             wp_id++;
             startTime = Time.time;
-            if (wp_id == waypoints.Length - 1)
+            if (wp_id == wps.Length - 1)
                 wp_id = 0;
         }
     }
@@ -74,27 +103,38 @@ public abstract class MovementType : ScriptableObject
     protected List<Vector3> extend_wp(GameObject[] waypoints)
     {
         List<Vector3> tpositions = new List<Vector3>();
-        tpositions.Add(waypoints[0].transform.position + (waypoints[0].transform.forward * 10));
-        for (int i = 0; i < waypoints.Length; i++)
-            tpositions.Add(waypoints[i].transform.position);
-        tpositions.Add(waypoints[waypoints.Length - 1].transform.position + (waypoints[waypoints.Length - 1].transform.forward * 10));
+        tpositions.Add(wps[0].transform.GetChild(0).position + (wps[0].transform.GetChild(0).forward * 10));
+        for (int i = 0; i < wps.Length; i++)
+            tpositions.Add(wps[i].transform.GetChild(0).position);
+        tpositions.Add(wps[wps.Length - 1].transform.GetChild(0).position + (wps[wps.Length - 1].transform.GetChild(0).forward * 10));
 
         return tpositions;
     }
 }
 
-[CreateAssetMenu(fileName = "LerpMovement", menuName = "MovementTypes/LerpMovement", order = 1)]
 public class LerpMovement : MovementType
 {
-    public LerpMovement() { }
+    public LerpMovement(GameObject[] wp, float spd)
+    {
+        wps = new GameObject[wp.Length];
+        wps = wp;
+        speed = spd;
+    }
 
     override public Vector3 moveCamera()
     {
         distCovered = (Time.time - startTime) * speed;
-        float fracJourney = distCovered / Vector3.Distance(waypoints[wp_id].transform.position, waypoints[wp_id + 1].transform.position);
 
-        Vector3 results = Vector3.Lerp(waypoints[wp_id].transform.position, waypoints[wp_id + 1].transform.position, fracJourney);
-        Debug.DrawLine(waypoints[wp_id].transform.position, waypoints[wp_id + 1].transform.position, Color.red);
+        // Creating temp vectors for the start and end
+        Vector3 p0 = wps[wp_id].transform.GetChild(0).position; 
+        Vector3 p1 = wps[wp_id+1].transform.GetChild(0).position;
+
+        // Calculating the precentage of the journey covered
+        float fracJourney = distCovered / Vector3.Distance(p0,p1);
+
+        // Calcuting the position along the journey
+        Vector3 results = Vector3.Lerp(p0,p1, fracJourney);
+        Debug.DrawLine(p0,p1, Color.red);
 
         //Check to see if the section has been done and move on to the next set of waypoints
         finishedSection(fracJourney);
@@ -103,23 +143,32 @@ public class LerpMovement : MovementType
     }
 }
 
-[CreateAssetMenu(fileName = "BezierMovement", menuName = "MovementTypes/BezierMovement", order = 2)]
 public class BezierMovement : MovementType
 {
-    public bool use_c1_handle_snapping = false; 
+    public BezierMovement(GameObject[] wp, float spd, bool use_c1) {
+        // Snaps the Next handle inline with the previous and main handles
+        if (use_c1)
+            c1_snap_handle(wp);
 
-    public BezierMovement() { }
+        wps = new GameObject[wp.Length];
+        wps = wp;
+
+        speed = spd;
+    }
 
     override public Vector3 moveCamera()
     {
         distCovered = (Time.time - startTime) * speed;
 
-        curveType = new BezierCurve(waypoints[wp_id].transform.position,
-                            waypoints[wp_id].transform.GetChild(1).transform.position,
-                            waypoints[wp_id + 1].transform.GetChild(0).transform.position,
-                            waypoints[wp_id + 1].transform.position);
-        curveType.DrawDebugCurve(debug_drawing_step, debug_duration);
+        // creating variables for the current positions and handles
+        Vector3 p0 = wps[wp_id].transform.GetChild(0).position;
+        Vector3 p1 = wps[wp_id+1].transform.GetChild(0).position;
+        Vector3 h0 = wps[wp_id].transform.GetChild(2).position;
+        Vector3 h1 = wps[wp_id + 1].transform.GetChild(1).position;
 
+        // Caluating the position along the curve
+        curveType = new BezierCurve(p0, h0, h1, p1);
+        curveType.DrawDebugCurve(debug_drawing_step, debug_duration);
         Vector3 results = curveType.Evaluate(distCovered);
 
         //Check to see if the section has been done and move on to the next set of waypoints
@@ -127,25 +176,44 @@ public class BezierMovement : MovementType
        
         return results;
     }
+
+    // Handles placing the next handle inline with the main and previous handles
+    private void c1_snap_handle(GameObject[] wps)
+    {
+        foreach(GameObject wp in wps)
+        {
+            // Get the vector between the main and previous handles
+            Vector3 controlVector = wp.transform.GetChild(0).position - wp.transform.GetChild(1).position;
+            //get the distance from the main and next handles 
+            float dist = Vector3.Distance(wp.transform.GetChild(0).transform.position, wp.transform.GetChild(2).transform.position);
+            // Reposition the next handle to be inline with the previous and main handles 
+            wp.transform.GetChild(2).transform.position = wp.transform.GetChild(0).transform.position + (controlVector.normalized * dist);
+        }
+    }
 }
 
-[CreateAssetMenu(fileName = "BSplineMovement", menuName = "MovementTypes/BSplineMovement", order = 3)]
 public class BSplineMovement : MovementType
 {
-
-    public BSplineMovement() { }
+    public BSplineMovement(GameObject[] wp, float spd) {
+        wps = new GameObject[wp.Length];
+        wps = wp;
+        speed = spd;
+    }
 
     override public Vector3 moveCamera()
     {
         distCovered = (Time.time - startTime) * speed;
 
+        // creating variables for the current positions and handles
+        Vector3 p0 = wps[wp_id].transform.GetChild(0).position;
+        Vector3 p1 = wps[wp_id + 1].transform.GetChild(0).position;
+        Vector3 h0 = wps[wp_id].transform.GetChild(2).position;
+        Vector3 h1 = wps[wp_id + 1].transform.GetChild(1).position;
+
         // Uses the control points to add the curve data
-        curveType = new B3SplineCurve(waypoints[wp_id].transform.position,
-                    waypoints[wp_id].transform.GetChild(1).transform.position,
-                    waypoints[wp_id + 1].transform.GetChild(0).transform.position,
-                    waypoints[wp_id + 1].transform.position);
+        curveType = new B3SplineCurve(p0, h0, h1, p1);
         curveType.DrawDebugCurve(debug_drawing_step, debug_duration);
- 
+
         Vector3 results = curveType.Evaluate(distCovered);
 
         //Check to see if the section has been done and move on to the next set of waypoints
@@ -155,23 +223,30 @@ public class BSplineMovement : MovementType
     }
 }
 
-
-[CreateAssetMenu(fileName = "HermitMovement", menuName = "MovementTypes/HermitMovement", order = 4)]
 public class HermitMovement : MovementType
 {
     public float angular_scalar = 30.0f;
 
-    public HermitMovement() { }
+    public HermitMovement(GameObject[] wp, float spd, float ang_scl) {
+        wps = new GameObject[wp.Length];
+        wps = wp;
+        speed = spd;
+        angular_scalar = ang_scl;
+    }
 
     override public Vector3 moveCamera()
     {
 
         distCovered = (Time.time - startTime) * speed;
-        curveType = new HermitCurve(waypoints[wp_id].transform.position,
-                                    waypoints[wp_id + 1].transform.position,
-                                    -waypoints[wp_id].transform.forward * angular_scalar,
-                                    waypoints[wp_id + 1].transform.forward * angular_scalar);
 
+        // Creating variables for the current set of the waypoints
+        Vector3 p0 = wps[wp_id].transform.GetChild(0).position;
+        Vector3 p1 = wps[wp_id+1].transform.GetChild(0).position;
+        Vector3 t0 = wps[wp_id].transform.GetChild(0).forward * angular_scalar;
+        Vector3 t1 = -wps[wp_id+1].transform.GetChild(0).forward * angular_scalar;
+
+        // Calculating the current position along the curve
+        curveType = new HermitCurve(p0,p1,t0,t1);
         Vector3 results = curveType.Evaluate(distCovered);
         curveType.DrawDebugCurve(debug_drawing_step, debug_duration);
 
@@ -182,21 +257,25 @@ public class HermitMovement : MovementType
     }
 }
 
-[CreateAssetMenu(fileName = "CatmullMovement", menuName = "MovementTypes/CatmullMovement", order = 5)]
 public class CatmullMovement : MovementType
 { 
-    public CatmullMovement() { }
+    public CatmullMovement(GameObject[] wp, float spd) {
+        wps = new GameObject[wp.Length];
+        wps = wp;
+        speed = spd;
+    }
 
     override public Vector3 moveCamera()
     {
         distCovered = (Time.time - startTime) * speed;
   
-        List<Vector3> tpositions = extend_wp(waypoints);
-        curveType = new CatmullRomCurve(tpositions[wp_id],
-                                tpositions[wp_id + 1],
-                                tpositions[wp_id + 2],
-                                tpositions[wp_id + 3]);
+        List<Vector3> tpositions = extend_wp(wps);
+        Vector3 p0 = tpositions[wp_id];
+        Vector3 p1 = tpositions[wp_id+1];
+        Vector3 p2 = tpositions[wp_id+2];
+        Vector3 p3 = tpositions[wp_id + 3];
 
+        curveType = new CatmullRomCurve(p0,p1,p2,p3);
         curveType.DrawDebugCurve(debug_drawing_step, debug_duration);
         Vector3 results = curveType.Evaluate(distCovered);
 
