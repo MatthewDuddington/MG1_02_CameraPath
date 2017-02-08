dbffc3313a1e27b7e5e3a3f8584a5332abb33b1e
Matthew Duddington <mdudd001@gold.ac.uk>
2017-02-08 07:21:58 +0000

    Replacing Unity LookAt function with own version; Adjusting hermit cushioning curve values; Started adding fix for ‘One PoI snap’ problem

--- Camera_Path_Unity/Assets/Scripts/CameraLook.cs
+++ Camera_Path_Unity/Assets/Scripts/CameraLook.cs
@@ -10,6 +10,9 @@ public class CameraLook : MonoBehaviour {
     
     private Curve cushioningCurve;
 
+    private bool onlyOnePoI = true;
+    private Vector3 imagineryPoI;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -21,14 +24,18 @@ public class CameraLook : MonoBehaviour {
 	void Update () {
 
 		// Check for camera being close enough to recieve rotation influence from each point of interest
-		float[] weights = new float[pois.Length];
-		for (int i = 0; i < pois.Length; i++) {
+		float[] weights = new float[pois.Length + 1];  // Extra space for balancing weight
+		weights[weights.Length - 1] = 0;  // Default the extra to 0 so it only influences if we need it
+		int weightCount = 0;
+
+		for (int i = 0; i < weights.Length - 1; i++) {
 			float distanceFromPoI = Vector3.Magnitude(pois[i].transform.position - transform.position);
-			// print (distanceFromPoI);
+
 			if (distanceFromPoI <= pois[i].AreaOfInterestRadius()) {
+				weightCount++;
 				float weight = distanceFromPoI / pois[i].AreaOfInterestRadius();
 				// Use Hermet cuve Y value to 'cushion' the approach and exit from PoIs
-				weight = R_Curve.hermit(Vector3.zero, new Vector3(1,1,0), new Vector3(3f,-0.5f,0), new Vector3(-3f,0.5f,0), weight).y;
+				weight = R_Curve.hermit(Vector3.zero, new Vector3(1,1,0), new Vector3(3f,0,0), new Vector3(-3f,0,0), weight).y;
 				weight = 1 - weight;
 				weights[i] = weight;
 			}
@@ -43,20 +50,50 @@ public class CameraLook : MonoBehaviour {
 			weightTotal += weight;
 		}
 
+//		if (weightCount == 1) {
+//			if (onlyOnePoI == false) {
+//				onlyOnePoI = true;
+//				imagineryPoI = PoIBlob.transform.position;
+//			}
+//			else {
+//				onlyOnePoI = false;
+//			}
+//
+//			weights[weights.Length - 1] = weightTotal;
+//			weightTotal += weightTotal;	
+//		}
+
 		// Normalise the weights
 		for (int i = 0; i < weights.Length; i++) {
 			weights[i] = weights[i] / weightTotal;
 		}
 
 		// Create compound Vector3 to look at
-		Vector3 compoundPoI = new Vector3();
+		Mathd.Vector3 compoundPoI = Mathd.Vector3.zero;
 		for (int i = 0; i < pois.Length; i++) {
-			compoundPoI += pois[i].transform.position * weights[i];
+			compoundPoI += new Mathd.Vector3(pois[i].transform.position * weights[i]);
 		}
 
 		// Look at the compound point
-		PoIBlob.transform.position = compoundPoI;
-		transform.LookAt(compoundPoI);
+		PoIBlob.transform.position = compoundPoI.toUnityVec3;
+		LookAt(compoundPoI);
     }
+
+//    IEnumerator RemoveImagineryInfluence() {
+//    	float timeToEnd = Time.time + 2.5f; // Disolve influence over 2.5sec
+//    	Vector2 
+//    	while (Time.time < timeToEnd) {
+//    		Vector2.Lerp(
+//    	}
+//    }
+
+    void LookAt(Mathd.Vector3 pointToLookAt) {
+    	Mathd.Vector3 lookAtVector = pointToLookAt - new Mathd.Vector3(transform.position);
+    	float angleBetweenVectors = Vector3.Dot(transform.right.normalized, lookAtVector.toUnityVec3);
+    	transform.Rotate(transform.up, angleBetweenVectors);
+
+    	//transform.rotation = new UnityEngine.Quaternion();
+    }
+
 
 }
